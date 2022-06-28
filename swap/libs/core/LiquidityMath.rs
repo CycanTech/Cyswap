9241a20f67e0c307867f29ae401de0a30abea0c8
kaiweicai <kaiweicai@163.com>
2022-06-29 01:27:23 +0800

    add liquidityMath test

--- swap/libs/core/LiquidityMath.rs
+++ swap/libs/core/LiquidityMath.rs
@@ -14,11 +14,52 @@ pub fn addDelta(x: u128, y: i128) -> u128 {
     // }
 
     if y < 0 {
-        z = x - u128::try_from(-y).unwrap();
-        assert!(z < x, "LS");
+        println!("x is :{:?}",x);
+        z = x.checked_sub(u128::try_from(-y).unwrap());
+        match z {
+            None=> panic!("LS"),
+            Some(r)=> return r,
+        }
     } else {
-        z = x + u128::try_from(y).unwrap();
-        assert!(z >= x, "LA");
+        z = x.checked_add(u128::try_from(y).unwrap());
+        match z {
+            None => panic!("LA"),
+            Some(r)=> return r,
+        }
     }
-    z
+}
+
+
+#[cfg(test)]
+mod LiquidMathTest {
+
+    use crate::core::LiquidityMath;
+
+    //reverts if denominator is 0
+    #[test]
+    fn testAddDelta() {
+        assert_eq!(LiquidityMath::addDelta(1, 0),1);
+        assert_eq!(LiquidityMath::addDelta(1, -1),0);
+        assert_eq!(LiquidityMath::addDelta(1, 1),2);
+    }
+
+    //2**128-15 + 15 overflows
+    #[test]
+    #[should_panic(expected = "LA")]
+    fn test_overflows_add1(){
+        LiquidityMath::addDelta(u128::MAX, 1);
+    }
+
+    #[test]
+    #[should_panic(expected = "LS")]
+    fn test_overflows_sub1(){
+        LiquidityMath::addDelta(0, -1);
+    }
+
+    //3 + -4 underflows
+    #[test]
+    #[should_panic(expected = "LS")]
+    fn test_overflows_3_sub_4(){
+        LiquidityMath::addDelta(3, -4);
+    }
 }
